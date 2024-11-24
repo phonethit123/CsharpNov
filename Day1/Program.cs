61215e06a4ee4b9063397bc753094abc53598df1
Phone Thit Min Tun <phonethitmin.mm@gmail.com>
2024-11-24 11:57:19 +0700

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ITC");
+
+            string name = "Ko Phyu";
+
+            byte age = 18;
+            byte days = 31;
+
+            Console.WriteLine(age);
+            Console.WriteLine(days);
+
+            double dou = 10.123;
+            float flo = 10.123f;
+            decimal dec = 10.123m;
+
+            Console.WriteLine(dou);
+            Console.WriteLine(flo);
+            Console.WriteLine(dec);
+
+            bool bo = true;
+            Console.WriteLine(bo); 
+            
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date.ToString("MM/dd/yyyy"));
+
+
+        }
+    }
+}
